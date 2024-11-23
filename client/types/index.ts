5d5167ccb87b42f9eeac136889a850e76107d633
samarbadriddin0v <sammibadriddinov@gmail.com>
2024-11-23 07:28:58 +0200

    №2. Browse page UI

--- client/types/index.ts
+++ client/types/index.ts
@@ -1,3 +1,19 @@
 export interface ChildProps {
 	children: React.ReactNode
 }
+
+export interface QueryProps {
+	params: string
+	key: string
+	value?: string | null
+}
+
+export interface IProduct {
+	title: string
+	category: string
+	price: number
+	image: string
+	excerpt: string
+	imageKey: string
+	_id: string
+}
